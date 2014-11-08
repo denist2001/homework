fad0489d0d4c9ccd0f65066fe73fcd898f681c6c
Denis Tiufanov <denist2001@list.ru>
2014-11-08 20:18:26 +0300

    Template HH site.

--- src/main/java/Resume.java
+++ src/main/java/Resume.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by d.tyufanov on 08.11.2014.
  */
 public class Resume {
+    PersonalData personalData = new PersonalData();
+    ProfessionData professionData = new ProfessionData();
+    List<EducationDescription> education = new ArrayList<EducationDescription>();
+    List<WorkDescription> experience = new ArrayList<WorkDescription>();
 }
---
+++ src/main/java/WorkDescription.java
@@ -0,0 +1,12 @@
+/**
+ * Created by d.tyufanov on 08.11.2014.
+ */
+public class WorkDescription {
+    int startMonth, finishMonth;
+    int startYear, finishYear;
+    String name;
+    String city;
+    String site;
+    String description;
+
+}
